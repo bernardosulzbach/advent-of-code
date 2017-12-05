a66292275ec697be280011c16e756d08092c75ba
Bernardo Sulzbach <mafagafogigante@gmail.com>
2017-12-05 03:15:58 -0200

    Solved Day 4

---
+++ src/main/kotlin/org/mafagafogigante/advent/Solution201704.kt
@@ -0,0 +1,56 @@
+package org.mafagafogigante.advent
+
+class Solution201704 {
+
+    fun getFirstSolution(): Int {
+        val lines = javaClass.getResource("/2017-04.txt").readText(Charsets.UTF_8).trim().split("\n")
+        var count = 0
+        for (line in lines) {
+            val set = HashSet<String>()
+            var failed = false
+            for (token in line.split(" ")) {
+                if (set.contains(token)) {
+                    failed = true
+                    break
+                }
+                set.add(token)
+            }
+            if (!failed) {
+                count++
+            }
+        }
+        return count
+    }
+
+    fun getSecondSolution(): Int {
+        val lines = javaClass.getResource("/2017-04.txt").readText(Charsets.UTF_8).trim().split("\n")
+        var count = 0
+        for (line in lines) {
+            val set = HashSet<String>()
+            var failed = false
+            for (token in line.split(" ")) {
+                val charArray = token.toCharArray()
+                charArray.sort()
+                val sortedToken = String(charArray)
+                if (set.contains(sortedToken)) {
+                    failed = true
+                    break
+                }
+                set.add(sortedToken)
+            }
+            if (!failed) {
+                count++
+            }
+        }
+        return count
+    }
+
+}
+
+fun main(args: Array<String>) {
+    val solutionSeventeenThree = Solution201704()
+    val firstSolution = solutionSeventeenThree.getFirstSolution()
+    val secondSolution = solutionSeventeenThree.getSecondSolution()
+    println(firstSolution)
+    println(secondSolution)
+}
