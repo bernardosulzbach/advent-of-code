701fec42cb8b2b209e7e69d34351e8496a6ad035
Bernardo Sulzbach <mafagafogigante@gmail.com>
2017-12-15 09:21:16 -0200

    Solved 2017 - 14

---
+++ src/main/kotlin/org/mafagafogigante/advent/Point.kt
@@ -0,0 +1,22 @@
+package org.mafagafogigante.advent
+
+class Point(val x: Int, val y: Int) {
+
+    fun getNeighbors(): Array<Point> = arrayOf(Point(x - 1, y), Point(x, y - 1), Point(x, y + 1), Point(x + 1, y))
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other?.javaClass != javaClass) return false
+        other as Point
+        if (x != other.x) return false
+        if (y != other.y) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = x
+        result = 31 * result + y
+        return result
+    }
+
+}
