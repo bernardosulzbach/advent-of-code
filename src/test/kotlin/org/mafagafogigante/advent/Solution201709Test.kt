1cce2f97cf7a5c5a1a875626805b247a1aaba590
Bernardo Sulzbach <mafagafogigante@gmail.com>
2017-12-20 16:44:05 -0200

    Should use internal classes for JUnit tests

--- src/test/kotlin/org/mafagafogigante/advent/Solution201709Test.kt
+++ src/test/kotlin/org/mafagafogigante/advent/Solution201709Test.kt
@@ -3,7 +3,7 @@ package org.mafagafogigante.advent
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
-class Solution201709Test {
+internal class Solution201709Test {
 
     @Test
     fun testRemoveGarbageShouldRemoveAllGarbage() {
