c2a26b86dd91e15446e222236e7d06298194b6f1
Bernardo Sulzbach <bernardo@bernardosulzbach.com>
2020-12-17 21:09:16 +0100

    Solved 2020-02

---
+++ 2020/src/02.cpp
@@ -0,0 +1,39 @@
+#include "ArgumentParser.hpp"
+#include "Math.hpp"
+#include "StandardInput.hpp"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv) {
+  try {
+    ArgumentParser argumentParser;
+    argumentParser.parseArguments(argc, argv);
+    auto const part = argumentParser.getAdditionalArgument(0);
+    std::ifstream stream(argumentParser.getPath());
+    int validPasswords = 0;
+    int a;
+    while (stream >> a) {
+      stream.ignore();
+      int b;
+      stream >> b;
+      char character;
+      stream >> character;
+      stream.ignore(2);
+      std::string password;
+      stream >> password;
+      auto const count = std::count(std::begin(password), std::end(password), character);
+      if (part == 1 && Interval(a, b).contains(count)) {
+        validPasswords++;
+      } else if (part == 2 && ((password[a - 1] == character) ^ (password[b - 1] == character)) != 0) {
+        validPasswords++;
+      }
+    }
+    std::cout << validPasswords << '\n';
+  } catch (const std::exception &exception) {
+    std::cout << "Threw: " << exception.what() << '\n';
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
--- common/src/Math.hpp
+++ common/src/Math.hpp
@@ -16,3 +16,16 @@ template <typename T> T greatestCommonDivisor(T a, T b) {
 template <typename T> T leastCommonMultiple(T a, T b) {
   return (a * b) / greatestCommonDivisor(a, b);
 }
+
+template <typename T> struct Interval {
+  Interval(T const intervalMin, T const intervalMax) noexcept : min(intervalMin), max(intervalMax) {
+  }
+
+  [[nodiscard]] bool contains(T const value) noexcept {
+    return min <= value && value <= max;
+  }
+
+private:
+  T min;
+  T max;
+};
