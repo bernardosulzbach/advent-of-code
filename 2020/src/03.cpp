86d79fc774e311569ab7bbe95870ff5561220e8e
Bernardo Sulzbach <bernardo@bernardosulzbach.com>
2020-12-17 23:38:41 +0100

    Solved 2020-03

---
+++ 2020/src/03.cpp
@@ -0,0 +1,42 @@
+#include "ArgumentParser.hpp"
+#include "Direction.hpp"
+#include "Point.hpp"
+#include "StandardInput.hpp"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv) {
+  try {
+    ArgumentParser argumentParser;
+    argumentParser.parseArguments(argc, argv);
+    auto const part = argumentParser.getAdditionalArgument(0);
+    auto stream = std::ifstream(argumentParser.getPath());
+    auto const lines = AoC::readVector<std::string>(stream);
+    auto const countTrees = [&lines](std::size_t const right, std::size_t const down) {
+      std::size_t trees = 0;
+      Point<std::size_t> point{0, 0};
+      while (true) {
+        point = point.move(Direction::Right, right).move(Direction::Up, down); // Up is positive.
+        if (lines.size() <= point.y) {
+          break;
+        }
+        point.x %= lines[point.y].size();
+        if (lines[point.y][point.x] == '#') {
+          trees++;
+        }
+      }
+      return trees;
+    };
+    if (part == 1) {
+      std::cout << countTrees(3, 1) << '\n';
+    } else {
+      std::cout << countTrees(1, 1) * countTrees(3, 1) * countTrees(5, 1) * countTrees(7, 1) * countTrees(1, 2) << '\n';
+    }
+  } catch (const std::exception &exception) {
+    std::cout << "Threw: " << exception.what() << '\n';
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
