cb9930138afcd6cb541b026c431377376c3db518
Bernardo Sulzbach <bernardo@bernardosulzbach.com>
2021-02-06 10:56:57 +0100

    Modernized 2019-07

--- 2019/src/07.cpp
+++ 2019/src/07.cpp
@@ -1,25 +1,17 @@
 #include "ArgumentParser.hpp"
 #include "Intcode.hpp"
 
-#include <fstream>
 #include <iostream>
+#include <numeric>
 #include <optional>
-#include <string>
 
 namespace AoC {
-constexpr int AmplifierCount = 5;
+using ValueType = Intcode::ValueType;
+static constexpr Size AmplifierCount = 5;
 
-void solvePartOne(const std::vector<Intcode::ValueType> &memory) {
-  std::optional<std::array<int, AmplifierCount>> bestPhaseConfiguration;
-  std::optional<int> bestOutputLevel;
-  std::array<int, AmplifierCount> phaseConfiguration{0, 1, 2, 3, 4};
-  do {
-    std::vector<Intcode> amplifiers;
-    for (int i = 0; i < 5; i++) {
-      amplifiers.emplace_back(memory);
-      amplifiers[i].addInput(phaseConfiguration[i]);
-    }
-    for (int i = 0; i < AmplifierCount; i++) {
+void run(std::vector<Intcode> &amplifiers, U32 const part) {
+  if (part == 1) {
+    for (Size i = 0; i < AmplifierCount; i++) {
       if (i == 0) {
         amplifiers[i].addInput(0);
       } else {
@@ -29,30 +21,12 @@ void solvePartOne(const std::vector<Intcode::ValueType> &memory) {
         throw std::runtime_error("Should have halted.");
       }
     }
-    const auto outputLevel = amplifiers[AmplifierCount - 1].getOutput();
-    if (!bestOutputLevel || outputLevel > bestOutputLevel.value()) {
-      bestPhaseConfiguration = phaseConfiguration;
-      bestOutputLevel = outputLevel;
-    }
-  } while (std::next_permutation(std::begin(phaseConfiguration), std::end(phaseConfiguration)));
-  std::cout << bestOutputLevel.value() << '\n';
-}
-
-void solvePartTwo(const std::vector<Intcode::ValueType> &memory) {
-  std::optional<std::array<int, AmplifierCount>> bestPhaseConfiguration;
-  std::optional<int> bestOutputLevel;
-  std::array<int, AmplifierCount> phaseConfiguration{5, 6, 7, 8, 9};
-  do {
-    std::vector<Intcode> amplifiers;
-    for (int i = 0; i < 5; i++) {
-      amplifiers.emplace_back(memory);
-      amplifiers[i].addInput(phaseConfiguration[i]);
-    }
+  } else {
     amplifiers[0].addInput(0);
     auto running = true;
     auto firstRun = true;
     while (running) {
-      for (int i = 0; i < AmplifierCount; i++) {
+      for (Size i = 0; i < AmplifierCount; i++) {
         if (!firstRun) {
           amplifiers[i].addInput(amplifiers[(i + AmplifierCount - 1) % AmplifierCount].getOutput());
         }
@@ -62,6 +36,21 @@ void solvePartTwo(const std::vector<Intcode::ValueType> &memory) {
         }
       }
     }
+  }
+}
+
+void solve(std::vector<Intcode::ValueType> const &memory, U32 const part) {
+  std::optional<std::array<ValueType, AmplifierCount>> bestPhaseConfiguration;
+  std::optional<ValueType> bestOutputLevel;
+  std::array<ValueType, AmplifierCount> phaseConfiguration;
+  std::iota(std::begin(phaseConfiguration), std::end(phaseConfiguration), part == 1 ? 0 : 5);
+  do {
+    std::vector<Intcode> amplifiers;
+    for (Size i = 0; i < AmplifierCount; i++) {
+      amplifiers.emplace_back(memory);
+      amplifiers[i].addInput(phaseConfiguration[i]);
+    }
+    run(amplifiers, part);
     const auto outputLevel = amplifiers[AmplifierCount - 1].getOutput();
     if (!bestOutputLevel || outputLevel > bestOutputLevel.value()) {
       bestPhaseConfiguration = phaseConfiguration;
@@ -73,11 +62,7 @@ void solvePartTwo(const std::vector<Intcode::ValueType> &memory) {
 
 void main(ArgumentParser const &argumentParser) {
   auto const memory = readMemory(argumentParser.getPath());
-  if (argumentParser.getPart() == 1) {
-    solvePartOne(memory);
-  } else {
-    solvePartTwo(memory);
-  }
+  solve(memory, argumentParser.getPart());
 }
 } // namespace AoC
 
