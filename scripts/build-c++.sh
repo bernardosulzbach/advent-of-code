a5e12be1a4a448b3deb02a4d9cdfb13477b58de8
Bernardo Sulzbach <bernardo@bernardosulzbach.com>
2021-02-07 12:23:52 +0100

    Added a Bash script to build all C++ code

---
+++ scripts/build-c++.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+DIRECTORY="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIRECTORY" ]]; then DIRECTORY="$PWD"; fi
+. "$DIRECTORY/include.sh"
+
+BUILD_DIRECTORY="scripted-build-gcc"
+
+build() {
+  if [[ $# -ne 1 ]]; then
+    echo "build() expects a single argument."
+    return 1
+  fi
+  cd "$1" || exit 1
+  mkdir -p "$BUILD_DIRECTORY" && cd "$BUILD_DIRECTORY" || exit 1
+  cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Debug ..
+  cmake --build . --parallel "$(nproc)"
+  cd ../..
+}
+
+build "common"
+build "2019"
+build "2020"
