d38ac1588a70a37c2de54d75640514c77d91bb31
Bernardo Sulzbach <bernardo@bernardosulzbach.com>
2021-02-07 14:39:40 +0100

    Moved our casts to Casts.hpp
    This was needed to prevent a cyclical include.
    
    However, it's also just a good idea to give casts a dedicated header.

---
+++ common/src/Casts.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Concepts.hpp"
+
+#include <boost/numeric/conversion/cast.hpp>
+
+#include <type_traits>
+
+namespace AoC {
+using Checked = std::true_type;
+using Unchecked = std::false_type;
+
+/**
+ * Checks if the cast preserves the value before performing it.
+ */
+template <Integral Target, Integral Source> constexpr Target checkedCast(Source const source) {
+  return boost::numeric_cast<Target>(source);
+}
+
+/**
+ * Casts an integral to its unsigned version.
+ *
+ * If checked, checks if the cast preserves the value before performing it.
+ */
+template <typename IsChecked, Integral Source>
+constexpr std::make_unsigned_t<Source>
+unsignedCast(Source const source) noexcept(std::is_same_v<IsChecked, Unchecked>) {
+  if constexpr (std::is_same_v<IsChecked, Checked>) {
+    return static_cast<std::make_unsigned_t<Source>>(source);
+  } else {
+    return checkedCast<std::make_unsigned_t<Source>>(source);
+  }
+}
+} // namespace AoC
