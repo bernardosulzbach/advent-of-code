b6fc82712dd9055d0ae2d43f0c377b1ec980a954
Bernardo Sulzbach <bernardo@bernardosulzbach.com>
2021-03-20 21:29:45 +0100

    Solved 2020-21

--- 2020/src/Runner.cpp
+++ 2020/src/Runner.cpp
@@ -3,7 +3,10 @@
 namespace AoC {
 [[nodiscard]] std::set<RunSpecification> generateRunSpecifications() noexcept {
   std::set<RunSpecification> runSpecifications;
-  for (U32 day = 1; day <= 19; day++) {
+  for (U32 day = 1; day <= 21; day++) {
+    if (day == 20) {
+      continue;
+    }
     for (U32 part = 1; part <= 2; part++) {
       runSpecifications.emplace(day, false, part, 1);
     }
