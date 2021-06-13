0d1b2f68d8d38ffe784ae71dd4c90358555f2fab
Bernardo Sulzbach <bernardo@bernardosulzbach.com>
2021-06-13 11:26:51 +0200

    Improved CMake/Settings.cmake
    Clang should use libc++ and we shouldn't assume we're on GCC or Clang.

--- common/CMake/Settings.cmake
+++ common/CMake/Settings.cmake
@@ -2,8 +2,17 @@ set(CMAKE_CXX_STANDARD 20)
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS 1)
 
-if(CMAKE_BUILD_TYPE MATCHES "Debug")
+if(CMAKE_BUILD_TYPE MATCHES "Debug" AND (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "GNU"))
   add_definitions(-DDEBUGGING)
   add_compile_options(-fsanitize=address -fsanitize=undefined -fsanitize=leak)
   add_link_options(-fsanitize=address -fsanitize=undefined -fsanitize=leak)
+endif()
+
+if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+  add_compile_options(-fdiagnostics-color=always) # Colors on Ninja.
+endif()
+
+if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+  add_compile_options(-stdlib=libc++)
+  add_link_options(-stdlib=libc++)
 endif()
