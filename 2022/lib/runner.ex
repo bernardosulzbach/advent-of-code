f63f8466aec9fa4c689cbc1e881ec21d01208d07
Bernardo Sulzbach <bernardo@bernardosulzbach.com>
2022-12-24 18:23:20 +0100

    Should enumerate the modules programmatically with Elixir

--- 2022/lib/runner.ex
+++ 2022/lib/runner.ex
@@ -1,46 +1,52 @@
 defmodule Runner do
   def run() do
-    [Day01, Day02, Day03, Day04, Day05, Day06]
-    |> Enum.with_index()
-    |> Enum.map(fn {module, index} -> {module, index + 1} end)
-    |> Enum.map(fn {module, day} ->
-      IO.puts("Day #{day}")
+    with {:ok, list} <- :application.get_key(:advent_of_code_2022, :modules) do
+      list
+      |> Enum.filter(fn module ->
+        module |> Atom.to_string() |> String.match?(~r/Day\d{2}/)
+      end)
+      |> Enum.sort()
+      |> Enum.map(fn module ->
+        day = module |> Atom.to_string() |> String.slice(-2..-1) |> String.to_integer()
+        IO.puts("Day #{day}")
 
-      1..2
-      |> Enum.map(fn part ->
-        IO.puts("  Part #{part}")
+        1..2
+        |> Enum.map(fn part ->
+          IO.puts("  Part #{part}")
 
-        ["Sample", "Puzzle"]
-        |> Enum.map(fn test_type ->
-          padded_day = day |> Integer.to_string() |> String.pad_leading(2, "0")
-          directory = "#{String.downcase(test_type)}s"
+          ["Sample", "Puzzle"]
+          |> Enum.map(fn test_type ->
+            padded_day = day |> Integer.to_string() |> String.pad_leading(2, "0")
+            directory = "#{String.downcase(test_type)}s"
 
-          actual_output =
-            module.run(
-              "input/#{directory}/#{padded_day}.txt",
-              part
-            )
+            actual_output =
+              module.run(
+                "input/#{directory}/#{padded_day}.txt",
+                part
+              )
 
-          with {:ok, expected_output} = File.read("output/#{directory}/#{padded_day}-#{part}.txt") do
-            passed = actual_output == String.slice(expected_output, 0..-2)
+            with {:ok, expected_output} =
+                   File.read("output/#{directory}/#{padded_day}-#{part}.txt") do
+              passed = actual_output == String.slice(expected_output, 0..-2)
 
-            IO.puts(
-              if passed do
-                IO.ANSI.green()
-              else
-                IO.ANSI.red()
-              end <>
-                "    #{test_type} " <>
+              IO.puts(
                 if passed do
-                  "passed."
+                  IO.ANSI.green()
                 else
-                  "failed!"
+                  IO.ANSI.red()
                 end <>
-                IO.ANSI.reset()
-            )
-          end
+                  "    #{test_type} " <>
+                  if passed do
+                    "passed."
+                  else
+                    "failed!"
+                  end <>
+                  IO.ANSI.reset()
+              )
+            end
+          end)
         end)
       end)
-    end)
+    end
   end
 end
